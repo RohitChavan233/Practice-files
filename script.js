a33a6f99b75ca95cfc87efb387002da46a2639da
RohitChavan233 <rkchavan2024@gmail.com>
2026-06-11 11:37:35 +0530

    Updated project

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-var user = {
-    name: 'rohit',
-    age: 20,
-    batch: 'cohot 3',
-    college: {
-        collegeName: "theem college",
-        students:500,
-        course : 'CSE'
+// const person = {
+//     name: "Rahul",
+//     age: 22,
+//     city: "Delhi"
+// }
 
-    },
-    skills: ['js', 'python', 'react', 'node']
-}
+// for (let a in person) {
+//     console.log(a + " : " + person[a]);
+// }
 
-var obj = JSON.stringify(user)
-var obj = JSON.parse(obj)
+
+// const persons = {
+//     name: "Rahul",
+//     age: 22,
+//     city: "Delhi"
+// };
+
+// for (let a in persons) {
+//     console.log(a);          // key
+//     console.log(persons[a]);  // value
+// }
+
+
+
+
+// var person = {
+//     name: "Rahul",
+//     age: 22,
+//     city: "Delhi"
+// };
+
+// console.log("email" in person);
+
+
+
+const obj1 = { a: 1, b: 2 }
+const obj2 = { c: 3, d: 4 }
+
+var obj = {...obj1, ...obj2}
 
 console.log(obj);
 
