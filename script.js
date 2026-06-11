64fbfbb6176102fb85e9735cbc890a195106f444
RohitChavan233 <rkchavan2024@gmail.com>
2026-06-11 11:01:03 +0530

    Updated project

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-
 var user = {
-    name : 'rohit',
-    age : 20,
-    batch : 'cohot 3',
-    
+    name: 'rohit',
+    age: 20,
+    batch: 'cohot 3',
+    college: {
+        collegeName: "theem college",
+        students:500,
+        course : 'CSE'
+
+    },
+    skills: ['js', 'python', 'react', 'node']
 }
+
+var obj = JSON.stringify(user)
+var obj = JSON.parse(obj)
+
+console.log(obj);
+
