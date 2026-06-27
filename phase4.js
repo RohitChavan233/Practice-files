897e3d79e8c894e150fffd2b7966c6f7d4f776a9
RohitChavan233 <rkchavan2024@gmail.com>
2026-06-27 20:11:10 +0530

    updated

--- phase4.js
+++ phase4.js
@@ -1,20 +1,32 @@
-var place = {
-    placeName: 'Office',
-    behavior: 'Proffesional',
-    sayHi: function(){
-        const insidePlace = {
-            placeName: 'Meeting room',
-            behavior: 'Roasting',
-            sayHiInforntOffriends:function(){
-                console.log(this.behavior);
-            },
-            sayHiInforntOfClients:()=>{
-                console.log(this.behavior);
-            },
-        }
+// var place = {
+//     placeName: 'Office',
+//     behavior: 'Proffesional',
+//     sayHi: function(){
+//         const insidePlace = {
+//             placeName: 'Meeting room',
+//             behavior: 'Roasting',
+//             sayHiInforntOffriends:function(){
+//                 console.log(this.behavior);
+//             },
+//             sayHiInforntOfClients:()=>{
+//                 console.log(this.behavior);
+//             },
+//         }
 
-        insidePlace.sayHiInforntOfClients()
-    },
+//         insidePlace.sayHiInforntOfClients()
+//     },
+// }
+
+// place.sayHi()
+
+
+function CreateBook (bookName, auth, Page){
+    this.bookName = bookName;
+    this.authorName = auth;
+    this.pages = Page ;
 }
 
-place.sayHi()
+let book = new CreateBook('Rich Dad Poor Dad', 'JK Rowling', 553)
+let book1 = new CreateBook('Rich Dad Poor Dad', 'JK Rowling', 553)
+console.log(book);
+console.log(book1);
