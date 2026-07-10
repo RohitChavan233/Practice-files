82e8117168f129d0bd9600ad358be949d1d80579
RohitChavan233 <rkchavan2024@gmail.com>
2026-07-10 23:10:42 +0530

    learn to change HTML thorugh JS

--- Day_2_DOM/script.js
+++ Day_2_DOM/script.js
@@ -1,6 +1,8 @@
+const element = document.querySelector('h1')
 const body = document.body
 
-const main = document.querySelector('main')
 
-console.log(main.childNodes)
-console.log(main.children)
+console.log(body.textContent)
+
+// element.textContent = "Hello Everyone "
+// console.log(element)
