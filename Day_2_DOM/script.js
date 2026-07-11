93eb0f31ea5e4f9dcde31c9ede23e247cd8d2209
RohitChavan233 <rkchavan2024@gmail.com>
2026-07-11 10:05:39 +0530

    finding classes in js

--- Day_2_DOM/script.js
+++ Day_2_DOM/script.js
@@ -1,8 +1,14 @@
-const element = document.querySelector('h1')
-const body = document.body
+let h1 = document.querySelector('#head1')
+// const var1 = document.body
+// console.log(h1)
+
+// to change the class name in js 
+// const hey = h1.classList.replace("heading", "Hello")
+// console.log(hey)
 
 
-console.log(body.textContent)
 
-// element.textContent = "Hello Everyone "
-// console.log(element)
+
+// is h1 has a class 
+const isClass = h1.classList.contains("heading")
+console.log(isClass)
