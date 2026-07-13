6ceb0caa8da9dbfb70a9be5d28ce7cf07c863095
RohitChavan233 <rkchavan2024@gmail.com>
2026-07-13 21:55:47 +0530

    how to take input

--- Day_3_DOM/script.js
+++ Day_3_DOM/script.js
@@ -1,8 +1,8 @@
 // attributes and properties
 
-const h3 = document.querySelector('#id1')
-const res = h3.getAttribute('class')
-console.log(res)
+// const h3 = document.querySelector('#id1')
+// const res = h3.getAttribute('class')
+// console.log(res)
 
 // h3.removeAttribute('class')
 
@@ -12,7 +12,21 @@ console.log(res)
 
 // console.log(h3.hasAttribute('class'))
 
-const userCart = document.querySelector('#user-cart')
-console.log(userCart)
+// const userCart = document.querySelector('#user-cart')
+// console.log(userCart)
 
-console.log(userCart.getAttribute('get-user'))
+// console.log(userCart.getAttribute('get-user'))
+
+
+
+
+
+
+// input.value vs input.getAttribute("value")
+
+const inp = document.querySelector('input')
+const btn = document.querySelector('button')
+
+btn.addEventListener('click', ()=>{
+    console.log(inp.value)
+})
