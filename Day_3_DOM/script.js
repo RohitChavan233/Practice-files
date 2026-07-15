db2b224bafd735991118899cacedc39b6c0c8351
RohitChavan233 <rkchavan2024@gmail.com>
2026-07-15 08:22:16 +0530

    updated

--- Day_3_DOM/script.js
+++ Day_3_DOM/script.js
@@ -47,4 +47,30 @@
 // main.appendChild(footer)
 // main.removeChild(span)
 
+const main = document.querySelector('main')
+
+const box1 = document.createElement('div')
+const box2 = document.createElement('div')
+const box3 = document.createElement('div')
+
+box1.classList.add('box')
+box2.classList.add('box')
+box2.style.backgroundColor = "yellow"
+box3.classList.add('box')
+box3.style.backgroundColor = 'blue'
+main.append(box1, box2)
+
+main.replaceChild(box3, box1)
+
+
+
+
+// box2.style.backgroundColor = "yellow"
+// box3.style.backgroundColor = "blue"
+
+// main.insertBefore(box2, box1)
+// main.prepend(box3)
+
+
+
 
