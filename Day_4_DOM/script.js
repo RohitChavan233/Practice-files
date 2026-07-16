1f73da396c2de4ce27984554163a3b5aaa3de08e
RohitChavan233 <rkchavan2024@gmail.com>
2026-07-16 08:25:49 +0530

    events priority

--- Day_4_DOM/script.js
+++ Day_4_DOM/script.js
@@ -1,5 +1,20 @@
 const btn = document.querySelector('button')
+const div = document.querySelector('div')
+const main = document.querySelector('main')
+const body = document.body
 
-btn.addEventListener('click', ()=>{
-    console.log(Event)
+btn.addEventListener('click', (events)=>{
+    console.log('button triggered')
+})
+
+div.addEventListener('click', (events)=>{
+    console.log('div triggered')
+})
+
+main.addEventListener('click', (events)=>{
+    console.log('main triggered')
+})
+
+body.addEventListener('click', (events)=>{
+    console.log('body triggered')
 })
