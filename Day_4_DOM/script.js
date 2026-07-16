bef4fee684f69afefa67b9328830998b898bd255
RohitChavan233 <rkchavan2024@gmail.com>
2026-07-16 19:33:54 +0530

    creating a form with js logic

--- Day_4_DOM/script.js
+++ Day_4_DOM/script.js
@@ -1,20 +1,36 @@
-const btn = document.querySelector('button')
-const div = document.querySelector('div')
-const main = document.querySelector('main')
-const body = document.body
+// const btn = document.querySelector('button')
+// const div = document.querySelector('div')
+// const main = document.querySelector('main')
+// const body = document.body
 
-btn.addEventListener('click', (events)=>{
-    console.log('button triggered')
-}, {capture:true})
+// btn.addEventListener('click', (events)=>{
+//     console.log('button triggered')
+// }, {capture:true})
 
-div.addEventListener('click', (events)=>{
-    console.log('div triggered')
-},{capture:true})
+// div.addEventListener('click', (events)=>{
+//     console.log('div triggered')
+// },{capture:true})
 
-main.addEventListener('click', (events)=>{
-    console.log('main triggered')
-},{capture:true})
+// main.addEventListener('click', (events)=>{
+//     console.log('main triggered')
+// },{capture:true})
 
-body.addEventListener('click', (events)=>{
-    console.log('body triggered')
-},{capture:true})
+// body.addEventListener('click', (events)=>{
+//     console.log('body triggered')
+// },{capture:true})
+
+
+
+const form = document.querySelector('form')
+const name = document.querySelector('#name')
+const email = document.querySelector('#email')
+
+form.addEventListener('submit', (events)=>{
+    events.preventDefault()
+    console.log(name.value)
+    console.log(email.value)
+    
+    // console.log('event reloads')
+
+
+})
