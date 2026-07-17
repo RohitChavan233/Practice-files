20917378ac6a0958c0017d4bdc50873af52b170a
RohitChavan233 <rkchavan2024@gmail.com>
2026-07-17 22:48:11 +0530

    added link url

--- Day_4_DOM/script.js
+++ Day_4_DOM/script.js
@@ -25,6 +25,7 @@ const form = document.querySelector('form')
 const inp1 = document.querySelector('#name')
 const inp2 = document.querySelector('#email')
 const user = document.querySelector('.users')
+const url = document.querySelector('#url')
 
 
 let usersData = [
@@ -83,12 +84,13 @@ form.addEventListener('submit', (events) => {
 
     let name = inp1.value
     let email = inp2.value
+    let link = url.value
 
     // if(name.trim() ==='' && email.trim() === '') return
 
     user.innerHTML += `<div class="user_card">
             <div class="img_box">
-                <img src="https://images.unsplash.com/photo-1783950023744-ee1ca8f55859?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image is here">
+                <img src="${link}}">
             </div>
             <div class="text">
                 <h3>Name:${name}</h3>
