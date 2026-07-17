4cba887f757f0ed246dd90da1b9de732fa31aca0
RohitChavan233 <rkchavan2024@gmail.com>
2026-07-17 20:42:19 +0530

    now form is working

--- Day_4_DOM/script.js
+++ Day_4_DOM/script.js
@@ -22,15 +22,27 @@
 
 
 const form = document.querySelector('form')
-const name = document.querySelector('#name')
-const email = document.querySelector('#email')
+const inp1 = document.querySelector('#name')
+const inp2 = document.querySelector('#email')
+const user = document.querySelector('.users')
 
 form.addEventListener('submit', (events)=>{
     events.preventDefault()
     
-    let name_ = name.value
-    let email_ = email.value
-    console.log(name_, email_)
+    let name = inp1.value
+    let email = inp2.value
+
+    // if(name.trim() ==='' && email.trim() === '') return
+
+    user.innerHTML += `<div class="user_card">
+            <div class="img_box">
+                <img src="https://images.unsplash.com/photo-1783950023744-ee1ca8f55859?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image is here">
+            </div>
+            <div class="text">
+                <h3>Name:${name}</h3>
+                <p>Email:${email}</p>
+            </div>
+        </div>`
 
     form.reset()
 
