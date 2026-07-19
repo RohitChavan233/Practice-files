20a2956ca421368865e46056b834cf3d6401c48b
RohitChavan233 <rkchavan2024@gmail.com>
2026-07-19 20:00:59 +0530

    add some changes in it

--- Day_5_DOM/script.js
+++ Day_5_DOM/script.js
@@ -9,3 +9,16 @@
 //     console.log(events.target)
 // })
 
+
+
+const main = document.querySelector('main')
+const btn = document.querySelector('button')
+const box = document.querySelector('.box')
+
+btn.addEventListener('click', ()=>{
+    const left_ = Math.random() * 50
+    const top_ = Math.random() * 50
+
+    box.style.left = `${left_}%`
+    box.style.top = `${top_}%`
+})
