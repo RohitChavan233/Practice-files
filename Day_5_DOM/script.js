4d1242c7e314da470bedfcd4f0dbb0280d6b6761
RohitChavan233 <rkchavan2024@gmail.com>
2026-07-20 22:13:01 +0530

    added a score card

--- Day_5_DOM/script.js
+++ Day_5_DOM/script.js
@@ -15,12 +15,14 @@
 const main = document.querySelector('main')
 const btn = document.querySelector('button')
 const timer = document.querySelector('#timer')
+const scoree = document.querySelector('#score')
 const overlay = document.querySelector('#overlay')
 const box = document.createElement('div')
 box.classList.add('box')
 
 let time = 0
 let interval
+let score = 0
 
 const randomColor = () => {
     let r = Math.floor(Math.random() * 256)
@@ -33,8 +35,8 @@ const randomColor = () => {
 const randomBox = () => {
     main.append(box)
 
-    let mainH = main.clientHeight-box.offsetHeight
-    let mainW = main.clientWidth-box.offsetWidth
+    let mainH = main.clientHeight - box.offsetHeight
+    let mainW = main.clientWidth - box.offsetWidth
 
     const left_ = Math.random() * mainH
     const top_ = Math.random() * mainW
@@ -51,8 +53,8 @@ btn.addEventListener('click', () => {
     interval = setInterval(() => {
         randomBox()
         randomColor()
-    time += 1
-    timer.textContent = time
+        time += 1
+        timer.textContent = time
 
     }, 1000);
 
@@ -60,4 +62,15 @@ btn.addEventListener('click', () => {
         clearTimeout(interval)
         overlay.style.display = 'flex'
     }, 10000);
+})
+
+box.addEventListener('click', () => {
+    score += 10
+
+    scoree.textContent = score
+    // interval = setInterval(() => {
+    //     randomBox()
+    //     randomColor()    
+
+    // }, 1000);
 })
