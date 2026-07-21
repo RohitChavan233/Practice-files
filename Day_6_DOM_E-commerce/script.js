d382f4777dd33bd327220de909b6e550b084a242
RohitChavan233 <rkchavan2024@gmail.com>
2026-07-21 19:55:34 +0530

    some changes in button init

---
+++ Day_6_DOM_E-commerce/script.js
@@ -0,0 +1,34 @@
+const createBtn = document.querySelector('#create')
+const formDiv = document.querySelector('.form')
+const close = document.querySelector('#close')
+const form = document.querySelector('form')
+
+const productArr = []
+
+createBtn.addEventListener('click', ()=>{
+    formDiv.style.display = 'flex'
+})
+
+close.addEventListener('click', ()=>{
+    formDiv.style.display = 'none'
+})
+
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+
+    let productName  = event.target[0].value
+    let description  = event.target[1].value
+    let price  = event.target[2].value
+    let image  = event.target[3].value
+
+
+    let obj = {
+        productName,
+        description,
+        price,
+        image,
+    }
+
+    productArr.push(obj)
+})
