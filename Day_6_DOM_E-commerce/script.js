4e66973fb8a7506119857a385f44b9377084c7a3
RohitChavan233 <rkchavan2024@gmail.com>
2026-07-23 22:14:35 +0530

    form is complete

--- Day_6_DOM_E-commerce/script.js
+++ Day_6_DOM_E-commerce/script.js
@@ -2,8 +2,39 @@ const createBtn = document.querySelector('#create')
 const formDiv = document.querySelector('.form')
 const close = document.querySelector('#close')
 const form = document.querySelector('form')
+const productDiv = document.querySelector('.products')
 
 const productArr = []
+
+// https://picsum.photos/300/300
+
+
+let ui = ()=>{
+    productDiv.innerHTML = ''
+    productArr.forEach((elem) => {
+        productDiv.innerHTML += `
+            <div class="products-card">
+                <div class="img">
+                    <img src="${elem.image}" alt="Random Image">
+                </div>
+                <div class="text">
+                    <h3>${elem.productName}</h3>
+                    <p>${elem.description}</p>
+                    <p>${elem.price}</p>
+                </div>
+
+                <div class="btns">
+                    <button id="update">Update</button>
+                    <button id="delete">Delete</button>
+                </div>
+            </div>`
+    });
+}
+
+
+
+
+
 
 createBtn.addEventListener('click', () => {
     formDiv.style.display = 'flex'
@@ -12,7 +43,6 @@ createBtn.addEventListener('click', () => {
 close.addEventListener('click', () => {
     formDiv.style.display = 'none'
 })
-
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -27,7 +57,6 @@ form.addEventListener('submit', (event) => {
         return
     }
 
-
     let obj = {
         productName,
         description,
@@ -36,9 +65,14 @@ form.addEventListener('submit', (event) => {
     }
 
     productArr.push(obj)
+    console.log(productArr)
+    ui()
+    // formDiv.style.display = 'none' 
 
-    // console.log(productArr)
+
+
     // console.log(obj)
-
     form.reset()
+    formDiv.style.display = 'none'
+
 })
